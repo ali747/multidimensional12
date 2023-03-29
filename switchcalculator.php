2ee7fa7116b0470f59a6187fc68a108e73899424
ali747 <razaafzal748@gmail.com>
2023-03-29 15:07:11 +0500

    Create switchcalculator.php

---
+++ switchcalculator.php
@@ -0,0 +1,22 @@
+<?php
+$grade = 'B';
+
+switch($grade) {
+    case 'A' :
+        printf("Excellent!\n" );
+        break;
+        case 'B' :
+            case 'C' :
+                printf("Well done\n" );
+                break;
+                case 'D' :
+                    printf("You passed\n" );
+                    break;
+                    case 'F' :
+                        printf("Better try again\n" );
+                        break;
+                        default :
+                        printf("Invalid grade\n" );
+                    }
+                    printf("Your grade is  %c\n", $grade );
+?>
