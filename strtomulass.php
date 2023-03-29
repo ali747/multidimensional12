2c58401bd409292490a33cc269c7cc267f5bb4f1
ali747 <razaafzal748@gmail.com>
2023-03-29 14:12:39 +0500

    Create strtomulass.php

---
+++ strtomulass.php
@@ -0,0 +1,25 @@
+<?php
+function superhero(array $arr)
+{
+    $res=array();
+    foreach($arr as $str)
+    {
+        $num=array();
+        for($i=1; $i<=strlen($str); $i++)
+        {
+            $num[]=$i;
+        }
+        //print_r($num);
+        $res[$str]=$num;
+        
+
+    }
+    return $res;
+}
+
+// Example usage:
+$superheroes = ["spider-man", "super-man", "iron-man"];
+//print_r($superheroes);
+$res=superhero($superheroes);
+//print_r($res);
+?>
